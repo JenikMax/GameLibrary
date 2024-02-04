34b6d547164d4b24b83844038951eb4a31e073c5
Evgeni Mahnach <e.mahnach@egartech.com>
2024-02-04 16:34:59 +0300

    modify pages logic

--- src/main/java/com/jenikmax/game/library/controller/view/LibraryViewController.java
+++ src/main/java/com/jenikmax/game/library/controller/view/LibraryViewController.java
@@ -1,5 +1,6 @@
 package com.jenikmax.game.library.controller.view;
 
+import com.jenikmax.game.library.model.dto.GameShortDto;
 import com.jenikmax.game.library.service.api.LibraryService;
 import com.jenikmax.game.library.service.scaner.api.ScanerService;
 import org.apache.logging.log4j.LogManager;
@@ -8,6 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.expression.Numbers;
+import org.thymeleaf.util.NumberUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class LibraryViewController {
@@ -21,9 +29,41 @@ public class LibraryViewController {
     }
 
     @GetMapping("/library")
-    public String main(Model model) {
+    public String main(Model model, @RequestParam(value = "page", defaultValue = "1") int page) {
         logger.info("Open library");
-        model.addAttribute("gameList",libraryService.getGameList());
+        List<GameShortDto> gameList = libraryService.getGameList();
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        gameList.addAll(gameList);
+        int pageSize = 12;
+        int totalPages = (gameList.size() + pageSize - 1) / pageSize;
+        int startIndex = (page - 1) * pageSize;
+        int endIndex = Math.min((startIndex + pageSize), gameList.size());
+
+        List<GameShortDto> paginatedGames = gameList.subList(startIndex, endIndex);
+
+        List<String> years = libraryService.getReleaseDates();
+        List<String> platforms = libraryService.getGamesPlatforms();
+        List<String> genres = libraryService.getGameGenres();
+        model.addAttribute("gameList",paginatedGames);
+        model.addAttribute("years", years);
+        model.addAttribute("platforms", platforms);
+        model.addAttribute("genres", genres);
+        model.addAttribute("page", page);
+        model.addAttribute("pages", initPages(page,totalPages));
+        model.addAttribute("totalPages", totalPages);
         return "libraryView";
     }
 
@@ -42,5 +82,10 @@ public class LibraryViewController {
         return "redirect:/library";
     }
 
+    private List<Integer> initPages(int page, int totalPage){
+        return Arrays.asList(NumberUtils.sequence(Math.max(1, page - 3),Math.min(totalPage, page + 3)));
+    }
+
+    // ${#numbers.sequence(Math.max(1, page - 3), Math.min(totalPages, page + 3))}"
 
 }
