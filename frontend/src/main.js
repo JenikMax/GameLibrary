465c23d1054a6ed214b62f9b4bf26fea01c45ba8
Evgeni Mahnach <e.mahnach@egartech.com>
2026-07-17 23:36:50 +0300

    доработана темная тема

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,37 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes'
+
+const WarmAura = definePreset(Aura, {
+  semantic: {
+    colorScheme: {
+      light: {
+        surface: {
+          0:   "#f5f3f0",
+          50:  "#edeae6",
+          100: "#e3e0db",
+          200: "#d5d1cb",
+          300: "#b0aba3",
+          400: "#8c877e",
+          500: "#6e6a63",
+          600: "#534f49",
+          700: "#3d3a36",
+          800: "#282623",
+          900: "#181716",
+          950: "#0c0b0a",
+        },
+        text: {
+          color: "{surface.600}",
+          hoverColor: "{surface.700}",
+          mutedColor: "{surface.500}",
+          hoverMutedColor: "{surface.600}",
+        },
+      }
+    }
+  }
+})
+
 import Tooltip from 'primevue/tooltip'
 import ToastService from 'primevue/toastservice'
 import ConfirmationService from 'primevue/confirmationservice'
@@ -18,7 +49,7 @@ app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: WarmAura,
     options: {
       darkModeSelector: '.app-dark'
     }
