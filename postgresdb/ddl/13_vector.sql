302687ed43ecb34d93e7291526c0f870f9ecede5
Evgeni Mahnach <e.mahnach@egartech.com>
2026-07-21 01:02:32 +0300

    исправление запуска

--- postgresdb/ddl/13_vector.sql
+++ postgresdb/ddl/13_vector.sql
@@ -1,10 +1,10 @@
 \c "game-library"
 
-CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA library;
 
-ALTER TABLE library.game_data ADD COLUMN IF NOT EXISTS embedding vector(384);
+ALTER TABLE library.game_data ADD COLUMN IF NOT EXISTS embedding library.vector(384);
 
 ALTER TABLE library.game_data ADD COLUMN IF NOT EXISTS description_en text;
 
 CREATE INDEX IF NOT EXISTS idx_game_data_embedding
-    ON library.game_data USING hnsw (embedding vector_cosine_ops);
+    ON library.game_data USING hnsw (embedding library.vector_cosine_ops);
